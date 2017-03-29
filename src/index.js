72b908d247c079c3bc7196263c726b3aea6ec514
Carolina de la Fuente <mdela160@fiu.edu>
2017-03-28 22:41:56 -0400

    One List Complete

--- src/index.js
+++ src/index.js
@@ -2,7 +2,31 @@ if (process.env.NODE_ENV !== 'production') require('./index.html')
 import './styles/screen.scss'
 
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  const input = document.querySelector('input')
+  const button = document.querySelector('button')
+  const list = document.querySelector('ul.list')
+  const form = document.querySelector('form')
+
+  button.addEventListener('click', (event) => {
+    event.preventDefault()
+    let item = input.value
+    const li = document.createElement('li')
+    li.textContent = item
+    list.appendChild(li)
+    form.reset()
+    let dblclk = false
+    li.addEventListener('click', () => {
+      setTimeout(() => {
+        if (!dblclk) {
+          li.style.textDecoration = 'line-through'
+        }
+      }, 300)
+    })
+    li.addEventListener('dblclick', (event) => {
+      dblclk = true
+      list.removeChild(li)
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', main)
